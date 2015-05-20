8336b172af6c11156c22856894bdb828469d346e
nicholrgUW <nicholrgUW@users.noreply.github.com>
2015-05-20 00:17:43 -0700

    Create Bidder.java
    Initial Bidder attempt. Full of stubs.

---
+++ src/Model/Bidder.java
@@ -0,0 +1,43 @@
+package Model;
+
+import java.util.List;
+
+public class Bidder {
+
+	
+	private int _id; //added after constructed
+	private String _nickName;
+//	private Map<Item, int>_itemsBidOn;
+	private List<Item> _itemsWon;
+	
+		
+	public Bidder (String theName /*,contact info*/, String theNickName){
+//		super(/*name, contact info*/);
+//		_name = theName;
+		_nickName = theName;
+	}
+	public void setid(int theID){
+		_id = theID;
+	}
+	
+	public int getid(){
+		return _id;
+	}
+	
+	public void setNickName(String theNick){
+		_nickName = theNick;
+	}
+	
+	public String getNickName(){
+		return _nickName;
+	}
+	
+	public void placeBid(Item theItem){
+		//SOMETHING
+	}
+	
+	public void buyOut(Item theItem){
+		//SOMETHING
+	}
+	
+}
