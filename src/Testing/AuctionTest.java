121bdd3a41e5533257ad1dd583b12b9d8dd5e9af
Mark <rarebot@yahoo.com>
2015-05-23 15:39:54 -0700

    add Auction test

---
+++ src/Testing/AuctionTest.java
@@ -0,0 +1,15 @@
+package Testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AuctionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	
+}
