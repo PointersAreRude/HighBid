ec4daaec53151e63624af1dd4a0e647a2cd6b3d2
Abigail Smith <abilynsmith@yahoo.com>
2015-05-27 22:54:38 -0700

    Most of the StatsGUI layout and functionality is working.  It's not all fully implemented, regarding taking in Bidders, Donors, and Items lists, but it won't take long to do that.  And no comments made yet, will do that at a later time!

--- src/View/StatsFrameTemp.java
+++ src/View/StatsFrameTemp.java
@@ -1,24 +1,72 @@
 package View;
 
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 
-public class StatsFrameTemp extends JFrame {
+import Model.Item;
+
+public class StatsFrameTemp<E> extends JFrame {
 
 	private final int WIDTH = 1200;
 	
 	private final int HEIGHT = 800;
+	
+	StatsPanel<E> myPanel;
+	
+	ArrayList<E> myList;
 	
 	public StatsFrameTemp() {
 		setTitle("Stats");
 		setSize(WIDTH, HEIGHT);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		createList();
+		myPanel = new StatsPanel<E>(myList);
+		add(myPanel);
 		
 		setUp();
+		//pack();
 	}
 	
 	private void setUp() {
 		
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void createList() {
+		myList = (ArrayList<E>) new ArrayList<Item>();
+		myList.add((E) new Item("Speakers", "Some Speakers", 20, 60, 98989898));
+		myList.add((E) new Item("Pillow", "A cozy blue pillow!", 5, 10, 76535796));
+		myList.add((E) new Item("Laptop", "Durable, long battery life, and a sleek case!", 50, 500, 112334534));
+		myList.add((E) new Item("Pen Set", "Black ink pens.", 2, 10, 556212354));
+		myList.add((E) new Item("Rubber Ducky", "A cute, yellow, squeeky ducky!", 1, 5, 7655432));
+		myList.add((E) new Item("Fresh apples", "a description", 20, 60, 78369));
+		myList.add((E) new Item("Teddy Bear", "beeeeeeeees", 20, 60, 8989875));
+		myList.add((E) new Item("Cornbread", "Some Speakers", 20, 60, 4000009));
+		myList.add((E) new Item("Dog Collor", "", 20, 60, 332211));
+		myList.add((E) new Item("Elephant statue", "", 20, 60, 432211));
+		myList.add((E) new Item("Froyo Maker", "", 20, 60, 54545454));
+		myList.add((E) new Item("Giant Cheese Wheel", "", 20, 60, 876641));
+		myList.add((E) new Item("Hair Salon Coupons", "", 20, 60, 397544));
+		myList.add((E) new Item("Igloo Maker", "", 20, 60, 543578000));
+		myList.add((E) new Item("Juggler's Guide for Dummy's", "", 20, 60, 300098));
+		myList.add((E) new Item("Kangaroo tracker", "", 20, 60, 287521));
+		myList.add((E) new Item("Nail Salon tickets", "", 20, 60, 452413));
+		myList.add((E) new Item("Opera Tickets", "", 20, 60, 52314111));
+		myList.add((E) new Item("Purse", "", 20, 60, 32456547));
+		myList.add((E) new Item("IQ Test", "", 20, 60, 45455767));
+		myList.add((E) new Item("Pet Frog", "", 20, 60, 777777));
+		myList.add((E) new Item("Rock-em Sock-em Robots game", "", 20, 60, 65656677));
+		myList.add((E) new Item("Sailboat", "", 20, 60, 8775421));
+		myList.add((E) new Item("Truffles", "", 20, 60, 01010100));
+		myList.add((E) new Item("Umbrella", "", 20, 60, 65543111));
+		myList.add((E) new Item("Violin", "", 20, 60, 39398279));
+		myList.add((E) new Item("Wallet", "", 20, 60, 878909));
+		myList.add((E) new Item("Xyliphone", "", 20, 60, 651123));
+		myList.add((E) new Item("Zebra plushie", "", 20, 60, 33445643));
 	}
 	
 }
--- src/View/StatsPanel.java
+++ src/View/StatsPanel.java
@@ -1,29 +1,202 @@
 package View;
 
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.ListCellRenderer;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
+import Model.Bidder;
+import Model.Donor;
 import Model.Item;
 
-public class StatsPanel extends JPanel implements ActionListener {
+public class StatsPanel<E> extends JPanel {
 
-	JList<Item> scroller;
+	private JScrollPane myScroller;
 	
-	JTextArea text;
+	private JList<E> myList;
 	
+	private JTextArea myText;
 	
+	private JLabel myLabel;
 	
-	public StatsPanel() {
-		scroller = new JList<Item>();
+	private JButton myBack;
+	
+	private E[] myArray;
+	
+	private JPanel myLabelPanel;
+	
+	private JPanel myListPanel;
+	
+	@SuppressWarnings("unchecked")
+	public StatsPanel(ArrayList<E> list) {
+		
+		if (!list.isEmpty() && list.get(0) instanceof Item) {
+			myArray = (E[]) new Item[list.size()];
+			
+		} else if (!list.isEmpty() && list.get(0) instanceof Bidder) {
+			myArray = (E[]) new Bidder[list.size()];
+			
+		} else if (!list.isEmpty() && list.get(0) instanceof Donor) {
+			myArray = (E[]) new Donor[list.size()];
+			
+		}
+		for (int i = 0; i < list.size(); i++) {
+			myArray[i] = list.get(i);
+		}
+		
+		setUp();
 		
 	}
+	
+	public void setUp() {
+		GridBagLayout bag = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		
+		setLayout(bag);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 1.0;
+		c.weighty = 0.1;
+		c.gridheight = 1;
+		c.gridwidth = 5;
+		myLabel = new JLabel("Items");
+		myLabel.setFont(MainFrame.FORM_LABEL_FONT);
+		bag.setConstraints(myLabel, c);
+		add(myLabel);
+		
+		//add(Box.createRigidArea(new Dimension(myLabel.getWidth(), myLabel.getHeight())));
+		
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.weighty = 0.0;
+		myBack = new JButton("Back");
+		myBack.setFont(MainFrame.FORM_TF_FONT);
+		bag.setConstraints(myBack, c);
+		add(myBack);
+		
+		c.gridheight = 50;
+		c.weighty = 2.0;
+		c.gridwidth = 1;
+		myList = new JList<E>(myArray);
+		myList.setCellRenderer(new MyCellRenderer());
+		myScroller = new JScrollPane(myList);
+		bag.setConstraints(myScroller, c);
+		add(myScroller);
+		
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		myText = new JTextArea();
+		myText.setEditable(false);
+		myText.setFont(MainFrame.FORM_TF_FONT);
+		myText.setText("Please select an option from the list.");
+		bag.setConstraints(myText, c);
+		add(myText);
+		setSize(1200, 800);
+		
+		myList.addListSelectionListener(new MyListSelectionListener(myText, myList));
+	}
+	
+//	public void paintComponent(Graphics aGraphics) {
+//		super.paintComponent(aGraphics);
+//        Graphics2D g2d = (Graphics2D) aGraphics;
+//        
+//        Shape rectangle = new Rectangle2D.Double(0, 0, getWidth(), getHeight());
+//        g2d.setPaint(Color.RED);
+//        g2d.setStroke(new BasicStroke(10));
+//        g2d.draw(rectangle);
+//	}
 
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+//	public void actionPerformed(ActionEvent arg0) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+	
+	private class MyListSelectionListener implements ListSelectionListener {
+		
+		private JTextArea myText;
+		
+		private JList<E> myList;
+		
+		public MyListSelectionListener (JTextArea aText, JList<E> aList) {
+			myText = aText;
+			myList = aList;
+		}
+		
+		public void valueChanged(ListSelectionEvent event) {
+			E selected = (E) myList.getSelectedValue();
+			
+			String text = "";
+			if (selected instanceof Item) {
+				Item item = (Item) selected;
+				text += item.getName() + "\n" + item.getDescription();
+				
+			} else if (selected instanceof Bidder) {
+				Bidder bidder = (Bidder) selected;
+				
+			} else if (selected instanceof Donor) {
+				Donor donor = (Donor) selected;
+				
+			}
+			myText.setText(text);
+			
+		}
+		
+	}
+	
+	private class MyCellRenderer extends JLabel implements ListCellRenderer<E> {
+
+		public Component getListCellRendererComponent(
+				JList<? extends E> list,
+				E value, 
+				int index, 
+				boolean isSelected, 
+				boolean cellHasFocus) {
+			String toDisplay = "";
+			if (value instanceof Item) {
+				Item theValue = (Item) value;
+				toDisplay = theValue.getName() + " : " + theValue.getQr();
+			} else if (value instanceof Bidder) {
+				Bidder theValue = (Bidder) value;
+				toDisplay = theValue.getFirstName() + " " + theValue.getLastName() + ":" + theValue.getid();
+			} else if (value instanceof Donor) {
+				Donor theValue = (Donor) value;
+				toDisplay = theValue.getFirstName() + " " + theValue.getLastName();
+			}
+			
+			setText(toDisplay);
+			if (isSelected) {
+	             setBackground(list.getSelectionBackground());
+	             setForeground(list.getSelectionForeground());
+	         } else {
+	             setBackground(list.getBackground());
+	             setForeground(list.getForeground());
+	         }
+	         setEnabled(list.isEnabled());
+	         setFont(new Font(list.getFont().getName(), list.getFont().getStyle(), 20));
+	         setOpaque(true);
+	         return this;
+		}
 		
 	}
 }
