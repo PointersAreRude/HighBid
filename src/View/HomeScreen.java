a4f842ae6d9e483b9541458b7c123ef5e9f0b7c0
Mark <rarebot@yahoo.com>
2015-05-28 15:51:25 -0700

    Update HomeScreen

--- src/View/HomeScreen.java
+++ src/View/HomeScreen.java
@@ -1,15 +1,118 @@
 package View;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class HomeScreen extends JLabel {
-
+public class HomeScreen extends JPanel implements ActionListener {
+	
 	/**
-	 * 
+	 * default serial
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Start screen's label
+	 */
+	private JLabel _label;
+	
+	/**
+	 * create button
+	 */
+	private JButton _ItemsAndDonorOptionBtn;
+	
+	/**
+	 * open button
+	 */
+	private JButton _registrationBtn;
+	
+	private JButton _viewAuctionInfo;
+	
+	/**
+	 * File chooser
+	 */
+	private JFileChooser fc = new JFileChooser();
+	
 
 	public HomeScreen() {
 		setSize(MainFrame.WIDTH, MainFrame.HEIGHT);
+		setLayout(null);
+		setComponents();
+		addComponents();
 	}
+
+	/**
+	 * Setting for each component in this panel.
+	 */
+	private void setComponents() {
+		// Set HighBid Label
+		_label = new JLabel("HighBid Home Page");
+		_label.setFont(new Font("Tahoma", 0, 70));
+		_label.setBounds((int)((MainFrame.WIDTH / 2)-300), 90,700,100);
+		_label.setForeground(Color.BLUE);
+		
+		// Set edit button
+		_ItemsAndDonorOptionBtn = new JButton("Items And Donors Option");
+		_ItemsAndDonorOptionBtn.setBounds((int)(MainFrame.WIDTH / 2) - 200, 270, 450, 60);
+		_ItemsAndDonorOptionBtn.setFont(MainFrame.BUTTON_FONT);
+		//_ItemsAndDonorOptionBtn.setContentAreaFilled(false);
+		//_ItemsAndDonorOptionBtn.setOpaque(false);
+		//_ItemsAndDonorOptionBtn.setBorderPainted(false);
+		_ItemsAndDonorOptionBtn.addActionListener(this);
+		
+		// Set registration button
+		_registrationBtn = new JButton("Registration");
+		_registrationBtn.setBounds((int)(MainFrame.WIDTH / 2) - 100, 370, 250, 60);
+		_registrationBtn.setFont(MainFrame.BUTTON_FONT);
+		//_registrationBtn.setContentAreaFilled(false);
+		_registrationBtn.addActionListener(this);
+		
+		//set stats button
+		_viewAuctionInfo = new JButton("View Auction Info");
+		_viewAuctionInfo.setBounds((int)(MainFrame.WIDTH / 2) - 150, 470, 350, 60);
+		_viewAuctionInfo.setFont(MainFrame.BUTTON_FONT);
+		//_viewAuctionInfo.setContentAreaFilled(false);
+		_viewAuctionInfo.addActionListener(this);
+	}
+
+	/**
+	 * Add the component to the panel.
+	 */
+	private void addComponents() {
+		this.add(_label);
+		this.add(_ItemsAndDonorOptionBtn);
+		this.add(_registrationBtn);
+		this.add(_viewAuctionInfo);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		JButton src = (JButton) e.getSource();
+		if(src == _ItemsAndDonorOptionBtn) {
+			MainFrame.CLAYOUT.show(MainFrame.CONTAINER, "CreatePanel");
+		} else {
+			// TODO: add open auction functionality
+			int returnVal = fc.showOpenDialog(null);
+			
+			if(returnVal == JFileChooser.APPROVE_OPTION ) {
+				File file = fc.getSelectedFile();
+			}
+		}
+	}
+	
+//	public static void main(String[] args){
+//		JFrame frame = new JFrame();
+//		HomeScreen te = new HomeScreen();
+//		frame.add(te);
+//		frame.setSize(MainFrame.WIDTH, MainFrame.HEIGHT);
+//		
+//		frame.setVisible(true);
+//	}
 }
