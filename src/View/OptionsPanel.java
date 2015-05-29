1b314a4c83e5614284721797a8c79ba3f3ac5ab3
Long Nguyen <conchutgidenho2290@yahoo.com>
2015-05-28 20:39:04 -0700

    added donor form

--- src/View/OptionsPanel.java
+++ src/View/OptionsPanel.java
@@ -7,13 +7,21 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
+
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
+
+import Model.Donor;
 
 public class OptionsPanel extends JPanel implements ActionListener{
 
@@ -36,6 +44,8 @@ public class OptionsPanel extends JPanel implements ActionListener{
 	
 	private JPanel _mainContainer;
 	
+	private JPanel container;
+	
 	private CardLayout _clayout;
 	
 	private JButton _addDonorBtn;
@@ -46,6 +56,19 @@ public class OptionsPanel extends JPanel implements ActionListener{
 	
 	private JButton _backBtn;
 	
+	private JButton _donorBtn;
+	
+	private JTextField _firstTF;
+	
+	private JTextField _lastTF;
+	
+	private JTextField _emailTF;
+	
+	private JTextField _addressTF;
+	
+	private JTextField _phoneTF;
+	
+	private JLabel _infoLabel;
 	
 	public OptionsPanel() {
 		setSize(MainFrame.WIDTH, MainFrame.HEIGHT);
@@ -58,17 +81,97 @@ public class OptionsPanel extends JPanel implements ActionListener{
 	}
 
 	private void setRemovePanel() {
-		// TODO Auto-generated method stub
+		_removePanel = new JPanel();
+		_removePanel.setBackground(Color.RED);
 		
 	}
 
 	private void setDonorPanel() {
-		// TODO Auto-generated method stub
+		container = new JPanel();
+		container.setLayout(null);
+		_donorPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.insets = new Insets(20,0,20,0);
+		gc.gridx = 0;
+		gc.gridy = 0;
+		gc.anchor = GridBagConstraints.LINE_END;
+		
+		JLabel first = new JLabel("First name: ");
+		first.setFont(MainFrame.FORM_LABEL_FONT);
+		_donorPanel.add(first, gc);
+		
+		gc.gridy++;
+		JLabel last = new JLabel("Last name: ");
+		last.setFont(MainFrame.FORM_LABEL_FONT);
+		_donorPanel.add(last, gc);
+		
+		gc.gridy++;
+		JLabel email = new JLabel("Email: ");
+		email.setFont(MainFrame.FORM_LABEL_FONT);
+		_donorPanel.add(email, gc);
+
+		gc.gridy++;
+		JLabel address = new JLabel("Address: ");
+		address.setFont(MainFrame.FORM_LABEL_FONT);
+		_donorPanel.add(address, gc);
+		
+		gc.gridy++;
+		JLabel phone = new JLabel("Phone: ");
+		phone.setFont(MainFrame.FORM_LABEL_FONT);
+		_donorPanel.add(phone, gc);
+				
+		gc.gridy = 0;
+		gc.gridx = 1;
+		_firstTF = new JTextField(20);
+		_firstTF.setPreferredSize(MainFrame.TF_DIMENSION);
+		_firstTF.setFont(MainFrame.FORM_TF_FONT);
+		_donorPanel.add(_firstTF, gc);
+		
+		gc.gridy++;
+		_lastTF = new JTextField(20);
+		_lastTF.setPreferredSize(MainFrame.TF_DIMENSION);
+		_lastTF.setFont(MainFrame.FORM_TF_FONT);
+		_donorPanel.add(_lastTF, gc);
+		
+		gc.gridy++;
+		_emailTF = new JTextField(20);
+		_emailTF.setPreferredSize(MainFrame.TF_DIMENSION);
+		_emailTF.setFont(MainFrame.FORM_TF_FONT);
+		_donorPanel.add(_emailTF, gc);
+		
+		gc.gridy++;
+		_addressTF = new JTextField(20);
+		_addressTF.setPreferredSize(MainFrame.TF_DIMENSION);
+		_addressTF.setFont(MainFrame.FORM_TF_FONT);
+		_donorPanel.add(_addressTF, gc);
+		
+		gc.gridy++;
+		_phoneTF = new JTextField(20);
+		_phoneTF.setPreferredSize(MainFrame.TF_DIMENSION);
+		_phoneTF.setFont(MainFrame.FORM_TF_FONT);
+		_donorPanel.add(_phoneTF, gc);
+		
+		gc.gridy++;
+		_donorBtn = new JButton("Add");
+		_donorBtn.setPreferredSize(new Dimension(100,60));
+		_donorBtn.setFont(_smallFont);
+		_donorBtn.addActionListener(this);
+		_donorPanel.add(_donorBtn, gc);
+		
+		_donorPanel.setBounds(250,120,500,500);
+		
+		_infoLabel = new JLabel();
+		_infoLabel.setBounds(350,600,400,100);
+		_infoLabel.setFont(_smallFont);
+		
+		container.add(_donorPanel);
+		container.add(_infoLabel);
 		
 	}
 
 	private void setItemPanel() {
-		// TODO Auto-generated method stub
+		_itemPanel = new JPanel();
+		_itemPanel.setBackground(Color.black);
 		
 	}
 
@@ -103,51 +206,75 @@ public class OptionsPanel extends JPanel implements ActionListener{
 		_backBtn.setFont(_smallFont);;
 		_backBtn.addActionListener(this);
 		
-		
 		_leftPanel.add(_addDonorBtn, gc);
 		gc.gridy++;
 		_leftPanel.add(_addItemBtn, gc);
 		gc.gridy++;
 		_leftPanel.add(_removeBtn, gc);
-		gc.gridy += 3;
+		gc.gridy++;
+		gc.weighty = 1;
 		_leftPanel.add(_backBtn, gc);
       
 	}
 
 	private void addComponents() {
-		// TODO Auto-generated method stub		
-		_itemPanel = new JPanel();
-		_itemPanel.setBackground(Color.black);
-		_donorPanel = new JPanel();
-		_donorPanel.setBackground(Color.CYAN);
-		_removePanel = new JPanel();
-		_removePanel.setBackground(Color.RED);
 		HomeScreen home = new HomeScreen();
 		
 		_clayout = new CardLayout();
 		_mainContainer = new JPanel();
 		_mainContainer.setLayout(_clayout);
 		_mainContainer.add(_itemPanel, "AddItem");
-		_mainContainer.add(_donorPanel, "AddDonor");
+		_mainContainer.add(container, "AddDonor");
 		_mainContainer.add(_removePanel, "Remove");
 		_mainContainer.add(home, "Home");
-		_clayout.show(_mainContainer, "AddItem");
+		_clayout.show(_mainContainer, "AddDonor");
 		
 		this.add(_leftPanel, BorderLayout.WEST);
 		this.add(_mainContainer, BorderLayout.CENTER);
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
+		String first = _firstTF.getText();
+		String last = _lastTF.getText();
+		String email = _emailTF.getText();
+		String address = _addressTF.getText();
+		String phone = _phoneTF.getText();
+		
+	    String[] arr = {first, last, email, address, phone};
+	    boolean isEmpty = false;
+		
 		JButton src = (JButton) e.getSource();
 		if(src == _addDonorBtn) {
 			_clayout.show(_mainContainer, "AddDonor");
 		} else if (src == _addItemBtn) {
 			_clayout.show(_mainContainer, "AddItem");
+			_infoLabel.setText("");
 		} else if (src == _removeBtn) {
 			_clayout.show(_mainContainer, "Remove");
-		} else {
+			_infoLabel.setText("");
+		} else if (src == _backBtn){
 			MainFrame.CLAYOUT.show(MainFrame.CONTAINER, "HomeScreen");
+			_infoLabel.setText("");
+		} else if (src == _donorBtn) {
+			for(int i = 0; i < arr.length; i++) {
+				if(arr[i].equals("")) {
+					isEmpty = true;
+					break;
+				}
+			}
+			if(isEmpty) {
+				_infoLabel.setText("Please enter all required fields.");
+			} else {
+				try {
+					Donor donor = new Donor(first, last, email, address, phone);
+					if(donor != null) {
+						MainFrame._auction.addDonor(donor);
+					}
+				} catch (Exception err) {
+					System.out.print(err);
+				}
+			}
 		}
 	}
 
